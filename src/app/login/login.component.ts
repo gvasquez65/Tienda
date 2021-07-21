0ca02675a7c0f0b1c835b5595a5ddf3730fc8d3a
German Vasquez H <gdvasquezh@gmail.com>
2021-07-21 08:37:10 -0500

    guard21072021

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -4,6 +4,7 @@ import { Component, OnInit, Input } from '@angular/core';
 import {Router, ActivatedRoute} from '@angular/router';
 // import { AppComponent } from "../app.component";
 import { AuthService } from '../services/auth.service';
+import { UsersService } from '../services/users.service';
 
 @Component({
   selector: "app-login",
@@ -17,20 +18,36 @@ export class LoginComponent implements OnInit {
   // @Input()
   // nombre = 'DesarrolloWeb.com';
 
-  constructor(private router: Router, private activatedRouter: ActivatedRoute,private service:AuthService) {}
+  constructor(private router: Router, private activatedRouter: ActivatedRoute,private service:AuthService, private userService: UsersService,) {}
   ngOnInit(): void {
     // throw new Error("Method not implemented.");
   }
 
-  login() {
-    this.service.setLogin('true');
-    this.router.navigateByUrl('/form');
+  // login() {
+  //   this.service.setLogin('true');
+  //   this.router.navigateByUrl('/form');
    
+  //   console.log(this.email);
+  //   console.log(this.password);
+  // }
+
+  //constructor(public userService: UsersService, public router: Router) {}
+
+  login() {
+    const user = {email: this.email, password: this.password};
+    this.userService.login(user).subscribe( data => {
+      console.log(data);
+      this.service.setLogin('true');
+     this.router.navigateByUrl('/form');
+    },
+    error => {
+            this.service.setLogin('false');
+             console.log(error);
+           }
+    );
     console.log(this.email);
     console.log(this.password);
   }
-
-  // constructor(public userService: UsersService, public router: Router) {}
 
   // login() {
   //   const user = { email: this.email, password: this.password };
